10bb3c29faddf469bfbe68ea0f9392d4ce14eb87
OmelchenkoStanislav <stasemelya2708@gmail.com>
2023-03-24 12:41:46 +0200

    Added Lection 1, Task 3 - Simple Calculator

---
+++ Lection_1/Task_3/main.py
@@ -0,0 +1,27 @@
+a = int(input("First number = "))
+b = int(input("Second number = "))
+operation = input("Select an operation |+|, |-|, |*|, |/|: ")
+if operation == "+":
+    print (a+b)
+elif operation == "-":
+    print (a-b)
+elif operation == "*":
+    print (a*b)
+elif operation == "/":
+    print (a/b)
+
+#a = int(input("First number = "))
+#b = int(input("Second number = "))
+#operation = input("Select an operation |+|, |-|, |*|, |/|: ")
+#match operation:
+        #case "+":
+            #print(a+b)
+        #case "-":
+            #print(a-b)
+        #case "*":
+            #print(a*b)
+        #case "/":
+            #print(a/b)
+
+
+
